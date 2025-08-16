fa7fa7cd311bab0ac5b54a90fd3ee329cb1690d5
Koshelev <koschelev00@mail.ru>
2025-08-16 10:22:00 +0300

    1

--- src/components/Cenroblock/Centroblock.tsx
+++ src/components/Cenroblock/Centroblock.tsx
@@ -9,6 +9,14 @@ import Card from "@/components/Card/Card";
 export default function Centerblock(){
   return (
     <>
+    <div>
+      <div>
+<h2>Начните заниматься спортом и улучшите качество жизни</h2>
+      </div>
+      <div className="">
+Измени своё тело за полгода!
+      </div>
+    </div>
     <Card/>
 
      
