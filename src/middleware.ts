f9842132460b7772cb99ac856c8708212e405077
Koshelev <koschelev00@mail.ru>
2025-09-13 22:45:31 +0300

    1

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,30 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// import { NextResponse } from "next/server";
+// import type { NextRequest } from "next/server";
 
-export function middleware(req: NextRequest) {
-  const token = req.cookies.get("token_global")?.value || req.cookies.get("_vercel_jwt")?.value;
-  const url = req.nextUrl.clone();
+// export function middleware(req: NextRequest) {
+//   const token = req.cookies.get("token_global")?.value || req.cookies.get("_vercel_jwt")?.value;
+//   const url = req.nextUrl.clone();
 
-  const publicPaths = ["/", "/course/:id"];
-
-
-  const isPrivate = !publicPaths.some((path) =>
-    req.nextUrl.pathname.match(new RegExp(`^${path.replace(":", "[^/]+")}$`))
-  );
-
-  if (isPrivate && !token) {
-    url.pathname = "/";
-    return NextResponse.redirect(url);
-  }
-
-  return NextResponse.next();
-}
+//   const publicPaths = ["/", "/course/:id"];
 
 
-export const config = {
-  matcher: [
-    "/profile",
-    "/courseWorkout/:courseId*",
+//   const isPrivate = !publicPaths.some((path) =>
+//     req.nextUrl.pathname.match(new RegExp(`^${path.replace(":", "[^/]+")}$`))
+//   );
+
+//   if (isPrivate && !token) {
+//     url.pathname = "/";
+//     return NextResponse.redirect(url);
+//   }
+
+//   return NextResponse.next();
+// }
+
+
+// export const config = {
+//   matcher: [
+//     "/profile",
+//     "/courseWorkout/:courseId*",
     
-  ],
-};
+//   ],
+// };
